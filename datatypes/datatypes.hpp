2dbdb6381916fccd07fd263b485f86736d262306
Jurge vanEijck <jurge.van.eijck@gmail.com>
2023-10-12 23:35:22 +0200

    introduction datatypes/datatypes.hpp

---
+++ datatypes/datatypes.hpp
@@ -0,0 +1,16 @@
+// This file contains the headers of all generated code for datatypes
+
+#ifndef _MRA_DATATYPES_DATATYPES_HPP
+#define _MRA_DATATYPES_DATATYPES_HPP
+
+#include "datatypes/ActionResult.pb.h"
+#include "datatypes/Ball.pb.h"
+#include "datatypes/Logging.pb.h"
+#include "datatypes/Obstacle.pb.h"
+#include "datatypes/Player.pb.h"
+#include "datatypes/Pose.pb.h"
+#include "datatypes/PosVel.pb.h"
+#include "datatypes/WorldState.pb.h"
+
+#endif
+

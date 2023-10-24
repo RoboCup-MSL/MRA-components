aefb095022d5222a9bbe80b435f9f1289962b903
Jurge vanEijck <jurge.van.eijck@gmail.com>
2023-10-24 23:33:48 +0200

    geometry/agnles: improve readability and comments

--- libraries/geometry/angles.cpp
+++ libraries/geometry/angles.cpp
@@ -1,7 +1,7 @@
 #include "angles.hpp"
 #include <cmath>
 
-// wrap rotation/angle to [0, 2pi) for standardized coordinates
+// wrap rotation/angle [rad] to [0, 2pi) for standardized coordinates
 double MRA::Geometry::wrap_2pi(double rot)
 {
     double b = 2 * M_PI;
@@ -10,15 +10,15 @@ double MRA::Geometry::wrap_2pi(double rot)
     return result;
 }
 
-// wrap rotation/angle to [-pi, pi) for subsequent abs()-and-compare
-double MRA::Geometry::wrap_pi(double rot)
+// wrap rotation/angle [rad] to [-pi, pi) for subsequent abs()-and-compare
+double MRA::Geometry::wrap_pi(double rotation_in_radians)
 {
-    double result = MRA::Geometry::wrap_2pi(rot);
+    double result = MRA::Geometry::wrap_2pi(rotation_in_radians);
     if (result >= M_PI) result -= 2 * M_PI;
     return result;
 }
 
-// relative angle in MSL-FCS, where 0 is facing opponent goal
+// relative angle [rad] in MSL-FCS, where 0 is facing opponent goal
 double MRA::Geometry::calc_facing_angle_fcs(double from_x, double from_y, double to_x, double to_y)
 {
     return MRA::Geometry::wrap_pi(atan2(to_y - from_y, to_x - from_x) - 0.5 * M_PI);
--- libraries/geometry/angles.hpp
+++ libraries/geometry/angles.hpp
@@ -5,13 +5,13 @@
 namespace MRA::Geometry
 {
 
-// wrap rotation/angle to [0, 2pi) for standardized coordinates
-double wrap_2pi(double rot);
+// wrap rotation/angle in radians to [0, 2pi) for standardized coordinates
+double wrap_2pi(double rotation_in_radians);
 
-// wrap rotation/angle to [-pi, pi) for subsequent abs()-and-compare
-double wrap_pi(double rot);
+// wrap rotation/angle in radians to [-pi, pi) for subsequent abs()-and-compare
+double wrap_pi(double rotation_in_radians);
 
-// calculate relative angle in MSL-FCS, where 0 is facing opponent goal
+// calculate relative angle in radians [-pi, pi) in MSL-FCS, where 0 is facing opponent goal
 // (which is 90 degrees rotated w.r.t. regular polar coordinate system)
 // example: when robot at x=2 takes a kickoff, the angle towards ball is 0.5*pi
 double calc_facing_angle_fcs(double from_x, double from_y, double to_x, double to_y);

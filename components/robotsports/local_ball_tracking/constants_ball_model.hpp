3df13f3f6248148e1033123318d465b926d96b44
Jurge vanEijck <jurge.van.eijck@gmail.com>
2023-11-13 07:26:54 +0100

    refactoring

--- components/robotsports/local_ball_tracking/constants_ball_model.hpp
+++ components/robotsports/local_ball_tracking/constants_ball_model.hpp
@@ -17,14 +17,13 @@ typedef enum {
 
 #define MAXHYP                  500             /* maximum number of hypotheses */
 #define MA_N                    20              /* number of samples in MA confidence */
-#define MAXHIST                 1               /* number of past generations to be logged */
 #define MAXFEATBUF              60              /* maximum number of stored features in a hypothesis */
 
-#define BM_SUCCESS               0              /* success */
-#define BM_ERROR_MAXHYP         -1              /* MAXHYP exceeded */
-#define BM_ERROR_NORM           -2              /* normalization error */
-#define BM_ERROR_NO_HYP         -3              /* no hypotheses */
-#define BM_ASSOCIATION_ERROR    -4              /* association not allowed */
-#define BM_FIT_ERROR            -5              /* fit not possible */
+const int BM_SUCCESS              =  0;              /* success */
+const int BM_ERROR_MAXHYP         = -1;              /* MAXHYP exceeded */
+const int BM_ERROR_NORM           = -2;              /* normalization error */
+const int BM_ERROR_NO_HYP         = -3;              /* no hypotheses */
+const int BM_ASSOCIATION_ERROR    = -4;              /* association not allowed */
+const int BM_FIT_ERROR            = -5;              /* fit not possible */
 
 #endif  // CONSTANTS_BM_H

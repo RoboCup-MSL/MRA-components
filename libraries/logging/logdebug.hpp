787207b29c7858ed8e75d3c1c4db9c141e0a49c8
Jan Feitsma <jan.feitsma@gmail.com>
2023-12-05 21:14:31 +0100

    logging: trying to figure out why tracing seems to stop halfway during testsuite

--- libraries/logging/logdebug.hpp
+++ libraries/logging/logdebug.hpp
@@ -5,17 +5,37 @@
 // uncomment to enable internal debug logging
 // this is intended only for developing libraries/logging
 
-//#define LOGDEBUG_ENABLED
-
+#define LOGDEBUG_ENABLED
 
 #ifdef LOGDEBUG_ENABLED
 #include <cstdio>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
+inline std::string getCurrentTimestamp() {
+    auto now = std::chrono::system_clock::now();
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::microseconds(microseconds)).count();
+
+    auto fractional_seconds = microseconds - (seconds * 1000000);
+
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
+    std::tm tm = *std::gmtime(&time);
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    oss << "." << std::setw(6) << std::setfill('0') << fractional_seconds;
+
+    return oss.str();
+}
 
 // Define the LOGDEBUG macro
 #define LOGDEBUG(fmt, ...) \
     do { \
         if constexpr (true) { \
-            std::printf("%s(%d) - ", __FILE__, __LINE__); \
+            std::printf("%s %s(%d) - ", getCurrentTimestamp().c_str(), __FILE__, __LINE__); \
             std::printf(fmt, ##__VA_ARGS__); \
             std::printf("\n"); \
             std::fflush(stdout); \

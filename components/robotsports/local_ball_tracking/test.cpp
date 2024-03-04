f680300644ca5930c11bc4269b989edc1aadd61a
Jurge vanEijck <jurge.van.eijck@gmail.com>
2024-03-04 02:55:45 +0100

    testcase from csvfile

--- components/robotsports/local_ball_tracking/test.cpp
+++ components/robotsports/local_ball_tracking/test.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <stdexcept>
 #include <random>
@@ -40,7 +41,67 @@ TEST(RobotsportsLocalBallTrackingTest, basicTick)
 }
 
 
+void execute_ball_traject_from_csv_file(std::string csv_filename, double confidence, double sigma)
+{
+    auto m = RobotsportsLocalBallTracking::RobotsportsLocalBallTracking();
+    auto output = RobotsportsLocalBallTracking::Output();
+    auto state = RobotsportsLocalBallTracking::State();
+    auto local = RobotsportsLocalBallTracking::Local();
+    auto params = m.defaultParams();
+    int error_value = 0;
 
+    ifstream inputFile(csv_filename);
+    if (not inputFile.is_open()) {
+       std::cerr << "inputfile could not be opened: " << csv_filename << std::endl;
+       return;
+    }
+
+    std::vector<std::vector<double>> input_data;
+    string line;
+    // skip first line
+    getline(inputFile, line);
+
+    while (getline(inputFile, line)) {
+      std::stringstream ss(line);
+      double value1, value2, value3;
+
+      if (not (ss >> value1 >> value2 >> value3)) {
+          std::cerr << "error reading line: " << line << std::endl;
+          continue;
+      }
+      input_data.push_back({value1, value2, value3});
+    }
+    inputFile.close();
+
+    // process the readed data
+    for (const auto& row : input_data)
+    {
+        auto input = RobotsportsLocalBallTracking::Input();
+        input.Clear();
+        google::protobuf::Timestamp timestamp = google::protobuf::util::TimeUtil::MillisecondsToTimestamp(row[0] * 1000);
+        auto  candidate = MRA::Datatypes::BallCandidate();
+        candidate.mutable_pose_fcs()->set_x(row[1]);
+        candidate.mutable_pose_fcs()->set_y(row[2]);
+        candidate.set_confidence(confidence);
+        candidate.set_sigma(sigma);
+        candidate.mutable_timestamp()->CopyFrom(timestamp);
+        candidate.set_source(1);
+        input.mutable_candidates()->Add()->CopyFrom(candidate);
+
+
+        error_value = m.tick(timestamp, input, params, state, output, local);
+        MRA_LOG_DEBUG("diagnostics: %s", MRA::convert_proto_to_json_str(local).c_str());
+        MRA_LOG_DEBUG("state: %s", MRA::convert_proto_to_json_str(state).c_str());
+        // Asserts for turn from middle to left position
+        EXPECT_EQ(error_value, 0);
+    }
+}
+
+TEST(RobotsportsLocalBallTrackingTest, omnivisionDataTest)
+{
+    MRA_TRACE_TEST_FUNCTION();
+    execute_ball_traject_from_csv_file("./testdata/robotsports_omnivision.csv", 0.8, 0.5);
+}
 
 int main(int argc, char **argv)
 {

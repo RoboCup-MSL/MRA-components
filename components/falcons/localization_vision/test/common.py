ce230afcc18a452794181930fe8a6da9ade3809a
Jan Feitsma <jan.feitsma@gmail.com>
2024-05-05 11:28:55 +0200

    falcons/localization_vision: tune.py: refactoring for reuse elsewhere

--- components/falcons/localization_vision/test/common.py
+++ components/falcons/localization_vision/test/common.py
@@ -6,6 +6,11 @@ from google.protobuf import json_format
 
 # our modules
 from components.falcons.localization_vision.interface import Input_pb2, Params_pb2, State_pb2, Output_pb2, Local_pb2
+try:
+    from components.falcons.localization_vision.test import pybind_ext
+except ImportError:
+    # maybe tick_call is not used, only Data
+    pass
 
 # data elements
 # mutable elements such as state get also a _before and _after variant, as stored in tick file
@@ -22,7 +27,7 @@ FILE_ELEMENTS = (
     'output',
     'local',
     'state_after')
-
+# these are all protobuf objects
 
 
 class Data():
@@ -64,4 +69,37 @@ class Data():
                 data = f.read(n)
                 getattr(self, key).MergeFromString(data)
 
+
+
+def tick_call(data : Data, check : bool = True) -> int:
+    """Call the localization_vision tick."""
+    # call tick
+    return_tuple = pybind_ext.tick(
+        data.input,
+        data.params,
+        data.state_before)
+    # check return code
+    return_code = return_tuple[0]
+    if check:
+        if return_code != 0:
+            raise RuntimeError('tick call failed with return code {:d}'.format(return_code))
+    # store results
+    data.state_after = return_tuple[1]
+    data.output = return_tuple[2]
+    data.local = return_tuple[3]
+    return return_code
+
+
+def tick_info(data : Data) -> str:
+    """Create an informative string (json) of what happened in a localization_vision tick."""
+    result = 'N/A'
+    if len(data.output.candidates):
+        c = data.output.candidates[0]
+        numberOfTries = data.local.numberOfTries[0]
+        result = '{"pose":{' \
+            + '"x":{:.6f},"y":{:.6f},"rz":{:.6f}'.format(c.pose.x, c.pose.y, c.pose.rz) \
+            + '},"confidence":' + '{:.6f}'.format(c.confidence) \
+            + ',"numberOfTries":' + '{:d}'.format(numberOfTries) \
+            + '}'
+    return result
 

054c1d4cf18a26460cb5467baadcbab2e8944a60
Jan Feitsma <jan.feitsma@gmail.com>
2024-07-15 17:08:35 +0200

    falcons/action_pass: implement the basic action, without any timer/state

--- components/falcons/action_pass/tick.cpp
+++ components/falcons/action_pass/tick.cpp
@@ -7,7 +7,7 @@
 using namespace MRA;
 
 // custom includes, if any
-// ...
+#include "geometry.hpp"
 
 
 int FalconsActionPass::FalconsActionPass::tick
@@ -21,10 +21,59 @@ int FalconsActionPass::FalconsActionPass::tick
 )
 {
     int error_value = 0;
+    MRA_LOG_TICK();
 
     // user implementation goes here
 
+    auto const ws = input.worldstate();
 
+    if (!ws.robot().hasball())
+    {
+        // TODO: robustness: use state - it can happen that the robot kicked the ball away, but it takes a tick or more for the ball to actually leave?
+        // in that case, functionally the pass was a success, so we should not produce FAILED
+        output.set_actionresult(MRA::Datatypes::FAILED);
+        return 0;
+    }
+
+    // initialize output before failure-mode checks
+    output.set_actionresult(MRA::Datatypes::RUNNING);
+
+    // configurables
+    // TODO: make configurable via Params
+    float angleAccuracyThreshold = 0.01;
+
+    // calculate rotation angle
+    MRA::Geometry::Position robotPos = ws.robot().position();
+    MRA::Geometry::Position passTargetPos = input.target().position();
+    passTargetPos.faceAwayFrom(robotPos);
+    MRA::Geometry::Position delta = passTargetPos - robotPos;
+    float remainingRotationAngle = delta.rz;
+
+    // TODO: intentional overshoot?
+
+    // set ballhandlers, always
+    output.set_bhenabled(true);
+    // TODO: disable just before shot, for improved accuracy/power?
+
+    // determine when to shoot
+    if (abs(remainingRotationAngle) < angleAccuracyThreshold)
+    {
+        // shoot, success
+        // TODO: calculate timeToKick, tune it?
+        output.set_dokick(true);
+        output.set_actionresult(MRA::Datatypes::PASSED);
+    }
+    else
+    {
+        // prepare, aim, running
+        *output.mutable_motiontarget()->mutable_position() = ws.robot().position();
+        output.mutable_motiontarget()->mutable_position()->set_rz(passTargetPos.rz);
+        *output.mutable_passtarget() = input.target().position();
+        output.set_actionresult(MRA::Datatypes::RUNNING);
+    }
+
+    // store diagnostics data
+    local.set_remainingrotationangle(remainingRotationAngle);
 
     return error_value;
 }

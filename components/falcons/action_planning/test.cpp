048fab3b6cbf8e99bc02b925efbcb11d981be887
Jan Feitsma <jan.feitsma@gmail.com>
2024-07-24 07:37:36 +0200

    falcons/action_planning: bring in some tests from falcons/code client side

--- components/falcons/action_planning/test.cpp
+++ components/falcons/action_planning/test.cpp
@@ -136,6 +136,422 @@ TEST_F(TestActionPlanner, TickStopBhEnabled)
     EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
 }
 
+TEST_F(TestActionPlanner, TickTestMoveActionAtTarget)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_rz(0.0);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_MOVE);
+    auto moveInputs = new FalconsActionPlanning::ActionMoveInputs();
+    moveInputs->mutable_target()->mutable_position()->set_x(1.0);
+    moveInputs->mutable_target()->mutable_position()->set_y(1.0);
+    moveInputs->mutable_target()->mutable_position()->set_rz(0.0);
+    moveInputs->set_ballhandlersenabled(true);
+    testActionInput.set_allocated_move(moveInputs);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints; // No motion setpoint
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    expectedSetpoints.mutable_move()->set_stop(true);
+    Datatypes::ActionResult expectedActionResult = Datatypes::PASSED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestMoveActionNotAtTarget)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_rz(0.0);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_MOVE);
+    auto moveInputs = new FalconsActionPlanning::ActionMoveInputs();
+    moveInputs->mutable_target()->mutable_position()->set_x(2.0);
+    moveInputs->mutable_target()->mutable_position()->set_y(2.0);
+    moveInputs->mutable_target()->mutable_position()->set_rz(0.5);
+    moveInputs->set_ballhandlersenabled(false);
+    testActionInput.set_allocated_move(moveInputs);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_x(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_y(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_rz(0.5);
+    expectedSetpoints.mutable_bh()->set_enabled(false);
+    Datatypes::ActionResult expectedActionResult = Datatypes::RUNNING;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestGetBallActionHasBall)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_hasball(true);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_GETBALL);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    Datatypes::ActionResult expectedActionResult = Datatypes::PASSED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestGetBallActionInactiveRobot)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_active(false);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_GETBALL);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_bh()->set_enabled(true); // TODO: this does not make much sense and likely gets overruled at lower levels in Falcons SW
+    Datatypes::ActionResult expectedActionResult = Datatypes::FAILED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestGetBallActionNoBall)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.clear_ball();
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_GETBALL);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    Datatypes::ActionResult expectedActionResult = Datatypes::FAILED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestGetBallActionTeammateHasBall)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    auto teammate = testWorldState.add_teammates();
+    teammate->set_hasball(true);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_GETBALL);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_bh()->set_enabled(true); // TODO: when accidentally scrumming with teammate, maybe better to disable?
+    Datatypes::ActionResult expectedActionResult = Datatypes::FAILED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestGetBallActionRunning)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_active(true);
+    testWorldState.mutable_ball()->mutable_position()->set_x(2.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(2.0);
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(1.0);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_GETBALL);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_x(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_y(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_rz(-0.78539816339744828);
+    Datatypes::ActionResult expectedActionResult = Datatypes::RUNNING;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestPassActionNoBall)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_hasball(false);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_PASS);
+    testActionInput.mutable_pass()->mutable_target()->mutable_position()->set_x(5.0);
+    testActionInput.mutable_pass()->mutable_target()->mutable_position()->set_y(5.0);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_shoot()->set_phase(FalconsActionAimedKick::SHOOT_PHASE_INVALID);
+    Datatypes::ActionResult expectedActionResult = Datatypes::FAILED;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestPassActionRunning)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // setup inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_hasball(true);
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(2.0);
+    testWorldState.mutable_robot()->mutable_position()->set_rz(0.0);
+    testWorldState.mutable_ball()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(2.1);
+
+    FalconsActionPlanning::ActionInputs testActionInput;
+    testActionInput.set_type(Datatypes::ACTION_PASS);
+    testActionInput.mutable_pass()->mutable_target()->mutable_position()->set_x(5.0);
+    testActionInput.mutable_pass()->mutable_target()->mutable_position()->set_y(6.0);
+
+    // set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInput);
+
+    // run tick
+    feedTick();
+
+    // setup expected outputs
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_x(1.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_y(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_rz(-0.78539816339744828);
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    expectedSetpoints.mutable_shoot()->set_type(FalconsActionPlanning::SHOOT_TYPE_PASS);
+    expectedSetpoints.mutable_shoot()->set_phase(FalconsActionAimedKick::SHOOT_PHASE_PREPARE);
+    expectedSetpoints.mutable_shoot()->set_pos_x(5.0);
+    expectedSetpoints.mutable_shoot()->set_pos_y(6.0);
+    Datatypes::ActionResult expectedActionResult = Datatypes::RUNNING;
+
+    // check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
+TEST_F(TestActionPlanner, TickTestPassActionStateTransitions)
+{
+    MRA_TRACE_TEST_FUNCTION();
+
+    // Setup initial inputs
+    Datatypes::WorldState testWorldState;
+    testWorldState.mutable_robot()->set_hasball(true);
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(2.0);
+    testWorldState.mutable_robot()->mutable_position()->set_rz(0.0);
+    testWorldState.mutable_ball()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(2.1);
+
+    FalconsActionPlanning::ActionInputs testActionInputs;
+    testActionInputs.set_type(Datatypes::ACTION_PASS);
+    testActionInputs.mutable_pass()->mutable_target()->mutable_position()->set_x(5.0);
+    testActionInputs.mutable_pass()->mutable_target()->mutable_position()->set_y(6.0);
+
+    // Set inputs in the planner
+    setWorldState(testWorldState);
+    setActionInputs(testActionInputs);
+
+    // Initial tick, robot has ball, go into PREPARE phase, aiming/rotating towards target
+    feedTick();
+
+    // Setup expected outputs for the first tick
+    FalconsActionPlanning::Setpoints expectedSetpoints;
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_x(1.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_y(2.0);
+    expectedSetpoints.mutable_move()->mutable_target()->mutable_position()->set_rz(-0.78539816339744828);
+    expectedSetpoints.mutable_bh()->set_enabled(true);
+    expectedSetpoints.mutable_shoot()->set_type(FalconsActionPlanning::SHOOT_TYPE_PASS);
+    expectedSetpoints.mutable_shoot()->set_phase(FalconsActionAimedKick::SHOOT_PHASE_PREPARE);
+    expectedSetpoints.mutable_shoot()->set_pos_x(5.0);
+    expectedSetpoints.mutable_shoot()->set_pos_y(6.0);
+    Datatypes::ActionResult expectedActionResult = Datatypes::RUNNING;
+
+    // Check the outputs after the first tick
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+
+    // For second tick, update inputs to finish the robot rotation, so the action will advance to DISCHARGE phase and kick the ball
+    testWorldState.mutable_robot()->mutable_position()->set_x(1.0);
+    testWorldState.mutable_robot()->mutable_position()->set_y(2.0);
+    testWorldState.mutable_robot()->mutable_position()->set_rz(-0.78539816339744828);
+    setWorldState(testWorldState);
+    feedTick();
+
+    // Setup expected outputs for the second tick
+    expectedSetpoints.mutable_move()->Clear();
+    expectedSetpoints.mutable_shoot()->Clear();
+    expectedSetpoints.mutable_shoot()->set_phase(FalconsActionAimedKick::SHOOT_PHASE_DISCHARGE);
+
+    // Check the outputs after the second tick
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+
+    // Tick: just check that the action transitions to COOLDOWN phase
+    feedTick();
+
+    // Check the outputs
+    expectedSetpoints.mutable_shoot()->set_phase(FalconsActionAimedKick::SHOOT_PHASE_COOLDOWN);
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+
+    // Tick: simulate that the ball has left the robot, action should still be RUNNING
+    testWorldState.mutable_ball()->mutable_position()->set_x(3.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(3.0);
+    testWorldState.mutable_robot()->set_hasball(false);
+    setWorldState(testWorldState);
+    feedTick();
+
+    // Check the outputs
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+
+    // Tick: simulate that the ball has arrived at target, action should finish PASSED
+    testWorldState.mutable_ball()->mutable_position()->set_x(5.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(6.0);
+    setWorldState(testWorldState);
+    feedTick();
+
+    // Check the outputs
+    FalconsActionPlanning::Local expectedDiagnostics;
+    expectedDiagnostics.mutable_action()->set_type(Datatypes::ACTION_PASS);
+    expectedDiagnostics.mutable_action()->mutable_pass()->set_aimerror(0.0);
+    expectedActionResult = Datatypes::PASSED;
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+    EXPECT_THAT(getDiagnostics(), EqualsProto(expectedDiagnostics));
+
+    // Alternate ending 1
+    // Tick: simulate that the ball slightly missed target, check reported aim error
+    testWorldState.mutable_ball()->mutable_position()->set_x(5.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(6.5);
+    setWorldState(testWorldState);
+    feedTick();
+
+    // Check the outputs
+    expectedDiagnostics.mutable_action()->set_type(Datatypes::ACTION_PASS);
+    expectedDiagnostics.mutable_action()->mutable_pass()->set_aimerror(0.058755815);
+    expectedActionResult = Datatypes::PASSED;
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+    EXPECT_THAT(getDiagnostics(), EqualsProto(expectedDiagnostics));
+
+    // Alternate ending 2
+    // Tick: simulate that the ball did not arrive at target and the cooldown expired, action should finish FAILED
+    testWorldState.mutable_ball()->mutable_position()->set_x(3.0);
+    testWorldState.mutable_ball()->mutable_position()->set_y(3.0);
+    setWorldState(testWorldState);
+    advanceTime(5.0);
+    feedTick();
+
+    // Check the outputs
+    expectedActionResult = Datatypes::FAILED;
+    EXPECT_THAT(getLastSetpoints(), EqualsProto(expectedSetpoints));
+    EXPECT_EQ(getLastActionResult(), expectedActionResult);
+}
+
 int main(int argc, char **argv)
 {
     InitGoogleTest(&argc, argv);

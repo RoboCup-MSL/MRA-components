2e1ebe5d8c88081c5d6cea23ea77fa236dcfee25
Jan Feitsma <jan.feitsma@gmail.com>
2024-08-03 10:11:10 +0200

    falcons/action_park: codegen

---
+++ components/falcons/action_park/FalconsActionPark.hpp
@@ -0,0 +1,119 @@
+// this file was produced by MRA-codegen.py from template_instance.hpp
+// it should NOT be modified by user
+
+#ifndef _MRA_FALCONS_ACTION_PARK_HPP
+#define _MRA_FALCONS_ACTION_PARK_HPP
+
+
+// component name definition goes on top
+// (when logging.hpp is used internally in a component, then component name may resolve to "unknown")
+#ifndef MRA_COMPONENT_NAME
+#define MRA_COMPONENT_NAME "FalconsActionPark"
+#endif
+#ifndef MRA_COMPONENT_FOLDER
+#define MRA_COMPONENT_FOLDER "falcons/action_park"
+#endif
+
+
+#include "abstract_interface.hpp"
+#include "params_loader.hpp"
+#include <google/protobuf/empty.pb.h>
+#include "logging.hpp"
+
+// generated protobuf types from interface of this component
+#include "FalconsActionPark_datatypes.hpp"
+
+
+
+
+namespace MRA::FalconsActionPark
+{
+
+typedef MRA::FalconsActionPark::Input InputType;
+typedef MRA::FalconsActionPark::Params ParamsType;
+typedef MRA::FalconsActionPark::State StateType;
+typedef MRA::FalconsActionPark::Output OutputType;
+typedef MRA::FalconsActionPark::Local LocalType;
+
+
+class FalconsActionPark: public MRAInterface<InputType, ParamsType, StateType, OutputType, LocalType>
+{
+public:
+    FalconsActionPark() {};
+    ~FalconsActionPark() {};
+
+    // user implementation
+    int tick(
+        google::protobuf::Timestamp timestamp,   // absolute timestamp
+        InputType  const           &input,       // input data, type generated from Input.proto
+        ParamsType const           &params,      // configuration parameters, type generated from Params.proto
+        StateType                  &state,       // state data, type generated from State.proto
+        OutputType                 &output,      // output data, type generated from Output.proto
+        LocalType                  &local        // local/diagnostics data, type generated from Local.proto
+    );
+
+    // make default configuration easily accessible
+    ParamsType defaultParams() const
+    {
+        return MRA::LoadDefaultParams<ParamsType>("components/falcons/action_park/interface/DefaultParams.json");
+    };
+
+    // allow omitting arguments, useful for testing and code brevity
+    int tick()
+    {
+        StateType s;
+        OutputType o;
+        LocalType l;
+        return tick(google::protobuf::util::TimeUtil::GetCurrentTime(), InputType(), defaultParams(), s, o, l);
+    };
+
+    int tick(
+        InputType  const &input,
+        OutputType       &output
+    )
+    {
+        StateType s;
+        LocalType l;
+        return tick(google::protobuf::util::TimeUtil::GetCurrentTime(), input, defaultParams(), s, output, l);
+    };
+
+    int tick(
+        InputType  const &input,
+        ParamsType const &params,
+        OutputType       &output
+    )
+    {
+        StateType s;
+        LocalType l;
+        return tick(google::protobuf::util::TimeUtil::GetCurrentTime(), input, params, s, output, l);
+    };
+
+    int tick(
+        InputType  const &input,
+        ParamsType const &params,
+        StateType        &state,
+        OutputType       &output,
+        LocalType        &local
+    )
+    {
+        return tick(google::protobuf::util::TimeUtil::GetCurrentTime(), input, params, state, output, local);
+    };
+
+}; // class FalconsActionPark
+
+
+// configuration handling
+inline ParamsType defaultParams()
+{
+    return FalconsActionPark().defaultParams();
+}
+inline ParamsType loadParams(std::string configFile)
+{
+    return MRA::LoadDefaultParams<ParamsType>(configFile);
+}
+
+} // namespace MRA::FalconsActionPark
+
+
+#endif
+

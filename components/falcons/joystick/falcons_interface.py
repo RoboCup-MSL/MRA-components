ffdd611ef31e4637f4d5e7a132c07a78032c72b8
Jan Feitsma <jan.feitsma@gmail.com>
2024-08-13 18:37:31 +0200

    joystick: joystick_falcons.py: implement most of mra_interface and falcons_interface

--- components/falcons/joystick/falcons_interface.py
+++ components/falcons/joystick/falcons_interface.py
@@ -2,14 +2,19 @@
 # use Falcons test interface to control robots
 
 # python imports
-# ...
+import os
+import json
+import logging
+from google.protobuf import json_format
 
 # local imports
 import mra_interface
 
 # falcons imports
-# TODO
-# import falconspy
+import falconspy # modifies sys.path to enable next imports
+import falconsrtdb
+import worldState # from Falcons WorldModel
+import EnvironmentField
 # import falconsrtdb
 # from sharedTypes import sharedTypes
 # write RTDB setpoints, etc.
@@ -20,24 +25,68 @@ import mra_interface
 class RobotInterface():
     def __init__(self, robotId):
         self.robotId = robotId
-        self.mra_interface = mra_interface.MRAInterface()
-        #self.mra_interface.params = # TODO load from falcons config json file as protobuf message
+        self.rtdbStore = falconsrtdb.FalconsRtDBStore(readonly=False)
+        self.rtdbStore.refresh_rtdb_instances()
+        self.homePos = [0, 0, 0]
+        self.goalPos = EnvironmentField.cEnvironmentField.getInstance().getFieldPOI(EnvironmentField.P_OPP_GOALLINE_CENTER)
+        self.worldState = worldState.WorldState(robotId)
+        self.mra_interface = mra_interface.MRAInterface(self)
+        # load params from json file (TODO: our pybind interface should provide a function for this)
+        action_planning_config_file = os.path.join(os.getenv('FALCONS_CODE_PATH'), 'config', 'ActionPlanningMRA.json')
+        with open(action_planning_config_file, 'r') as file:
+            json_data = json.load(file)
+        json_format.ParseDict(json_data, self.mra_interface.params)
+        # disable regular teamplay-driven execution
+        self.rtdbPut('MATCH_MODE', False)
 
-    def handle_packet(self, packet):
-        print(packet)
+    def __del__(self):
+        # restore regular teamplay-driven execution
+        self.rtdbPut('MATCH_MODE', True)
+
+    def handle_packet(self, packet : dict) -> None:
+        logging.debug('controller_packet: ' + str(packet))
         # set input
+        self.mra_interface.input.Clear()
         self.update_worldstate(self.mra_interface.input.worldState)
         self.mra_interface.set_input_action(packet)
         # call MRA actionplanning
-        r = self.mra_interface.call_action_planning()
-        # dispatch setpoints
-        if r == 0:
-            self.handle_setpoints(self.mra_interface.data.output)
-        # TODO: if action finished (either PASSED or FAILED), then go idle? packet producer does not know this
+        if len(packet['action']) > 0:
+            r = self.mra_interface.call_action_planning()
+            # dispatch setpoints
+            if r == 0:
+                self.handle_setpoints(self.mra_interface.output)
+            # TODO: if action finished (either PASSED or FAILED), then go idle? packet producer does not know this
 
     def handle_setpoints(self, setpoints):
-        print(setpoints)
+        pass
 
     def update_worldstate(self, world_state):
-        # TODO: get worldstate from RTDB as MRA WorldState object
-        world_state.robot.position.x = 3.0
+        self.worldState.update() # read from RTDB
+        self.worldState.toMRA(world_state)
+        logging.debug('worldstate: ' + str(json_format.MessageToJson(world_state, indent=None)))
+
+    def resolveTarget(self, target):
+        if target == 'home':
+            return self.homePos
+        elif target == 'goal':
+            return self.goalPos
+        elif target == 'nearestObstacle':
+            xy = self.worldState.nearestObstacle()
+            return (xy.x, xy.y, 0)
+        elif target == 'nearestTeammember':
+            xy = self.worldState.nearestTeammember()
+            return (xy.x, xy.y, 0)
+        else:
+            return target
+
+    def getBallHandlersEnabled(self) -> bool:
+        r = self.rtdbGet('BALLHANDLERS_SETPOINT', timeout=None)
+        if r != None:
+            return r.value
+        return False
+
+    def rtdbPut(self, key, value):
+        return self.rtdbStore.put(self.robotId, key, value)
+
+    def rtdbGet(self, key, *args, **kwargs):
+        return self.rtdbStore.get(self.robotId, key, *args, **kwargs)
--- components/falcons/joystick/mra_interface.py
+++ components/falcons/joystick/mra_interface.py
@@ -12,7 +12,8 @@ from datatypes import ActionType_pb2
 
 
 class MRAInterface():
-    def __init__(self):
+    def __init__(self, robot_interface):
+        self.robot_interface = robot_interface
         self.input = Input_pb2.Input()
         self.params = Params_pb2.Params() # client should overrule from config json file, or get error code 1 at first tick
         self.state = State_pb2.State()
@@ -25,10 +26,36 @@ class MRAInterface():
         if packet['action'] == '':
             return
         # convert to enum action value
+        # TODO: implement all actions after interface is agreed upon
         action_str = 'ACTION_' + packet['action'].upper()
         self.input.action.type = getattr(ActionType_pb2, action_str)
-        # TODO: resolve strings to coordinates using mra_input.worldstate
-        # TODO: switch on packet['action'] and assign subfield accordingly
+        # switch on packet['action'] and assign subfield accordingly
+        if packet['action'] == 'stop':
+            self.input.action.stop.ballHandlersEnabled = self.robot_interface.getBallHandlersEnabled()
+        elif packet['action'] == 'move':
+            # TODO self.input.action.move.motionTarget.position = self.robot_interface.resolveTarget(packet['args']['target'])
+            self.input.action.move.motionType = 0 # TODO dribble?
+            self.input.action.move.ballHandlersEnabled = self.robot_interface.getBallHandlersEnabled()
+        elif packet['action'] == 'kick':
+            pass
+        elif packet['action'] == 'pass':
+            pos = getattr(self.input.action, 'pass').target.position
+            pos.x, pos.y, pos.rz = self.robot_interface.resolveTarget(packet['args']['target'])
+        elif packet['action'] == 'shoot':
+            self.input.action.shoot.target.position = self.robot_interface.resolveTarget(packet['args']['target'])
+        elif packet['action'] == 'lob':
+            self.input.action.lob.target.position = self.robot_interface.resolveTarget(packet['args']['target'])
+        elif packet['action'] == 'getball':
+            self.input.action.getball.motionType = 0
+            self.input.action.getball.radius = packet['args']['radius']
+        elif packet['action'] == 'shield':
+            self.input.action.shield.CopyFrom(ActionShieldInputs())
+        elif packet['action'] == 'keeper':
+            self.input.action.keeper.CopyFrom(ActionKeeperInputs())
+        elif packet['action'] == 'intercept':
+            self.input.action.intercept.CopyFrom(ActionInterceptInputs())
+        elif packet['action'] == 'park':
+            self.input.action.park.CopyFrom(MRA.FalconsActionPark.Input())
 
     def call_action_planning(self, check : bool = True) -> int:
         """Call the action_planning tick."""

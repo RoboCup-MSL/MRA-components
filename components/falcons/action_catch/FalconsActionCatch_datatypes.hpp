2fbe419501f562f1ff30d6f1ba5eb2e4ad73d0ec
Jan Feitsma <jan.feitsma@gmail.com>
2024-09-22 14:09:59 +0200

    falcons/action_catch: codegen

---
+++ components/falcons/action_catch/FalconsActionCatch_datatypes.hpp
@@ -0,0 +1,15 @@
+// this file was produced by MRA-codegen.py from template_datatypes.hpp
+// it should NOT be modified by user
+
+#ifndef _MRA_FALCONS_ACTION_CATCH_DATATYPES_HPP
+#define _MRA_FALCONS_ACTION_CATCH_DATATYPES_HPP
+
+#include "components/falcons/action_catch/interface/Input.pb.h"
+#include "components/falcons/action_catch/interface/Params.pb.h"
+#include "components/falcons/action_catch/interface/State.pb.h"
+#include "components/falcons/action_catch/interface/Output.pb.h"
+#include "components/falcons/action_catch/interface/Diagnostics.pb.h"
+
+
+#endif
+

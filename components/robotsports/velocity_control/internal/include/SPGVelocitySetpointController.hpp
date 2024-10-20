d2143003545871b338cd66649381632ab7e27664
Jurge vanEijck <jurge.van.eijck@gmail.com>
2024-10-20 17:39:02 +0200

    Robotsports Velocity Control - introduction with use of ruckig trajectory generation lib

---
+++ components/robotsports/velocity_control/internal/include/SPGVelocitySetpointController.hpp
@@ -0,0 +1,58 @@
+/*
+ * SPGVelocitySetpointController.hpp
+ *
+ *  Created on: Oct, 2020
+ *      Author: Erik Kouters
+ */
+
+#ifndef ROBOTSPORTS_VELOCITYCONTROL_SPGVELOCITYSETPOINTCONTROLLER_HPP_
+#define ROBOTSPORTS_VELOCITYCONTROL_SPGVELOCITYSETPOINTCONTROLLER_HPP_
+
+// own package
+#include "VelocitySetpointControllers.hpp"
+
+// MRA-libraries
+#include "MRAbridge.hpp"
+
+// external
+//#include <ReflexxesAPI.h>
+#include <ruckig/ruckig.hpp>
+
+struct SpgLimits
+{
+    float vx;
+    float vy;
+    float vRz;
+    float ax;
+    float ay;
+    float aRz;
+};
+
+class SPGVelocitySetpointController2 : public AbstractVelocitySetpointController
+{
+public:
+    SPGVelocitySetpointController2();
+    ~SPGVelocitySetpointController2();
+    bool calculate(VelocityControlData &data);
+
+private:
+    bool calculateSPG(VelocityControlData& data, SpgLimits const &spgLimits, Position2D& resultPosition, Velocity2D &resultVelocity);
+    bool isDofAccelerating(const VelocityControlData &data, const Velocity2D& resultVelocity, int dof, float threshold);
+
+    // Position SPG
+    bool calculatePosXYRzPhaseSynchronized(VelocityControlData& data, SpgLimits const &spgLimits, Position2D& resultPosition, Velocity2D &resultVelocity);
+    bool calculatePosXYPhaseSynchronized(VelocityControlData& data, SpgLimits const &spgLimits, Position2D& resultPosition, Velocity2D &resultVelocity);
+    bool calculatePosRzNonSynchronized(VelocityControlData& data, SpgLimits const &spgLimits, Position2D& resultPosition, Velocity2D &resultVelocity);
+
+    // Velocity SPG
+    bool calculateVelXYRzPhaseSynchronized(VelocityControlData& data, SpgLimits const &spgLimits, Position2D& resultPosition, Velocity2D &resultVelocity);
+
+    // internal data stored for open loop
+    Position2D m_deltaPositionRCS;
+    Velocity2D m_currentVelocityRCS;
+    Velocity2D m_targetVelocityRCS;
+
+};
+
+#endif
+
---
+++ components/robotsports/velocity_control/internal/include/VelocityControlData.hpp
@@ -0,0 +1,60 @@
+/*
+ * VelocityControlData.hpp
+ *
+ *  Created on: May 2023
+ *      Author: Jan Feitsma
+ */
+
+#ifndef ROBOTSPORTS_VELOCITYCONTROL_VELOCITYCONTROLDATA_HPP_
+#define ROBOTSPORTS_VELOCITYCONTROL_VELOCITYCONTROLDATA_HPP_
+
+// MRA libraries
+#include "MRAbridge.hpp"
+
+// forward declaration
+class AbstractVelocitySetpointController;
+
+namespace MRA::internal::RVC
+{
+
+// this struct is used (r/w) by every algorithm
+struct VelocityControlData
+{
+    // MRA interface
+    MRA_timestamp      timestamp;
+    MRA_InputType      input;
+    MRA_ParamsType     config;
+    MRA_StateType      state;
+    MRA_DiagnosticsType diagnostics;
+    MRA_OutputType     output;
+
+    // while running the sequence of algorithms, this flag may be raised
+    bool done;
+
+    // which controller to use, determined by the SelectVelocityController algorithm
+    std::shared_ptr<AbstractVelocitySetpointController> controller;
+
+    // set the limits based on full configuration and input
+    MRA::RobotsportsVelocityControl::Limits limits;
+
+    // internal variables
+    int num_algorithms_executed;
+    MRA::RobotsportsVelocityControl::ControlModeEnum controlMode;
+    Position2D currentPositionFcs;
+    Velocity2D currentVelocityFcs;
+    Position2D targetPositionFcs;
+    Velocity2D targetVelocityFcs;
+    Position2D previousPositionSetpointFcs;
+    Velocity2D previousVelocitySetpointFcs;
+
+    // result
+    Velocity2D resultVelocityRcs;
+
+};
+
+} // namespace MRA::internal::RVC
+
+using namespace MRA::internal::RVC; //HACK
+
+#endif
+

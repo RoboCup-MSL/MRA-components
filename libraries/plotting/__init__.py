302ed57d33d56e6d4892af90fea02986375edd4a
Jan Feitsma <jan.feitsma@gmail.com>
2024-10-21 10:36:17 +0200

    libraries/plotting: add plot_target, a grey ghost robot

--- libraries/plotting/__init__.py
+++ libraries/plotting/__init__.py
@@ -3,5 +3,6 @@ from .field import plot as plot_field
 from .ball import plot as plot_ball
 from .robot import plot as plot_robot
 from .obstacle import plot as plot_obstacle
+from .target import plot as plot_target
 
-__all__ = ['plot_worldstate', 'plot_field', 'plot_robot', 'plot_ball', 'plot_obstacle']
+__all__ = ['plot_worldstate', 'plot_field', 'plot_robot', 'plot_ball', 'plot_obstacle', 'plot_target']

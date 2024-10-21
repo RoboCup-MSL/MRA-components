75cac3e06bcd7beb034aede40c4ce74dc0d58d61
Jan Feitsma <jan.feitsma@gmail.com>
2024-10-21 12:53:58 +0200

    falcons:action_keeper: implement basic intersection logic and configuration

--- components/falcons/action_keeper/tick.cpp
+++ components/falcons/action_keeper/tick.cpp
@@ -7,8 +7,24 @@
 using namespace MRA;
 
 // custom includes, if any
-// ...
+#include "geometry.hpp"
 
+
+void checkParams(FalconsActionKeeper::ParamsType const &params)
+{
+    if (params.ballspeedthreshold() == 0)
+    {
+        throw std::runtime_error("ballspeedthreshold must not be zero");
+    }
+    if (params.captureradius() == 0)
+    {
+        throw std::runtime_error("captureradius must not be zero");
+    }
+    if (params.maxmovex() == 0)
+    {
+        throw std::runtime_error("maxmovex must not be zero");
+    }
+}
 
 int FalconsActionKeeper::FalconsActionKeeper::tick
 (
@@ -21,11 +37,78 @@ int FalconsActionKeeper::FalconsActionKeeper::tick
 )
 {
     int error_value = 0;
+    MRA_LOG_TICK();
+
+    output.Clear();
+    diagnostics.Clear();
 
     // user implementation goes here
 
+    try
+    {
+        // Check parameters (some values must not be zero)
+        checkParams(params);
 
+        // Always return RUNNING, this action does not use PASSED or FAILED in its interaction with teamplay
+        output.set_actionresult(MRA::Datatypes::ActionResult::RUNNING);
+
+        // Check if keeper needs to respond to a ball
+        // * using ball speed
+        // * using ball direction
+
+        // Check if the ball speed is above the threshold
+        MRA::Geometry::Velocity ball_velocity = input.worldstate().ball().velocity();
+        double ball_speed = ball_velocity.size();
+        bool ballmovingfastenough = ball_speed > params.ballspeedthreshold();
+        diagnostics.set_ballmovingfastenough(ballmovingfastenough);
+        if (!ballmovingfastenough)
+        {
+            return error_value;
+        }
+
+        // Check if the ball is moving towards the robot
+        MRA::Geometry::Position robot_position(input.worldstate().robot().position());
+        MRA::Geometry::Velocity ball_velocity_rcs = ball_velocity;
+        ball_velocity_rcs.transformFcsToRcs(robot_position);
+        bool ballmovingtowardsrobot = ball_velocity_rcs.y < 0;
+        diagnostics.set_ballmovingtowardsrobot(ballmovingtowardsrobot);
+        if (!ballmovingtowardsrobot)
+        {
+            return error_value;
+        }
+
+        // Calculate interception point
+        // Use keeper horizontal line, do not rotate towards ball (as action catch does)
+        MRA::Geometry::Point goalline_point_1(-params.maxmovex(), params.baseliney());
+        MRA::Geometry::Point goalline_point_2(params.maxmovex(), params.baseliney());
+        MRA::Geometry::Position ball_position(input.worldstate().ball().position());
+        MRA::Geometry::Point ball_point(input.worldstate().ball().position().x(), input.worldstate().ball().position().y());
+        MRA::Geometry::Point ball_point_inf = ball_point + ball_velocity * 1000;
+        MRA::Geometry::Point intersect_point;
+        int intersect_result = intersect(goalline_point_1, goalline_point_2, ball_point, ball_point_inf, false, &intersect_point);
+        if (intersect_result != 1)
+        {
+            // No intersection found, ball out of range
+            return error_value;
+        }
+
+        // Set the calculated interception point as the motion target, facing the ball
+        MRA::Geometry::Position robot_target_position(intersect_point.x, intersect_point.y);
+        robot_target_position.rz = 0.0; // facing forward into the field
+        output.mutable_motiontarget()->mutable_position()->set_x(robot_target_position.x);
+        output.mutable_motiontarget()->mutable_position()->set_y(robot_target_position.y);
+        output.mutable_motiontarget()->mutable_position()->set_rz(robot_target_position.rz);
+    }
+    catch (const std::exception& e)
+    {
+        MRA_LOG_ERROR("ERROR: Caught a standard exception: %s", e.what());
+        error_value = -1;
+    }
+    catch (...)
+    {
+        MRA_LOG_ERROR("ERROR: Caught an unknown exception.");
+        error_value = -1;
+    }
 
     return error_value;
 }
-

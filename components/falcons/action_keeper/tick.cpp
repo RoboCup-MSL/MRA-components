c98c3d03e91e99c2494233c65b7eca9b9b50013f
Jan Feitsma <jan.feitsma@gmail.com>
2024-10-24 20:42:51 +0200

    falcons/action_keeper: extend implementation to cover full gameplay basics, but not yet advanced behavior made by Jeffrey

--- components/falcons/action_keeper/tick.cpp
+++ components/falcons/action_keeper/tick.cpp
@@ -26,6 +26,19 @@ void checkParams(FalconsActionKeeper::ParamsType const &params)
     }
 }
 
+double clip_x(double target_x, double limit_x)
+{
+    if (target_x > limit_x)
+    {
+        return limit_x;
+    }
+    else if (target_x < -limit_x)
+    {
+        return -limit_x;
+    }
+    return target_x;
+}
+
 int FalconsActionKeeper::FalconsActionKeeper::tick
 (
     google::protobuf::Timestamp timestamp,   // absolute timestamp
@@ -46,11 +59,47 @@ int FalconsActionKeeper::FalconsActionKeeper::tick
 
     try
     {
+        double target_rz = 0.0; // FCS=0, facing forward into the field
+
         // Check parameters (some values must not be zero)
         checkParams(params);
 
         // Always return RUNNING, this action does not use PASSED or FAILED in its interaction with teamplay
         output.set_actionresult(MRA::Datatypes::ActionResult::RUNNING);
+
+        // Homing routine
+        // phase 1: from a position far away to a position in front of the goal, using homePosYoffset
+        // phase 2: from general vicinity of the goal to the middle of the goalie base line
+        MRA::Geometry::Position robot_position(input.worldstate().robot().position());
+        MRA::Geometry::Position home_position(0, params.baseliney() + params.homeposyoffset(), 0);
+        double distance_to_home = (robot_position - home_position).size();
+        if (distance_to_home > params.homedistance())
+        {
+            output.mutable_motiontarget()->mutable_position()->set_x(home_position.x);
+            output.mutable_motiontarget()->mutable_position()->set_y(home_position.y);
+            output.mutable_motiontarget()->mutable_position()->set_rz(target_rz);
+            // done, ignore the ball
+            return error_value;
+        }
+        else
+        {
+            output.mutable_motiontarget()->mutable_position()->set_x(0);
+            output.mutable_motiontarget()->mutable_position()->set_y(params.baseliney());
+            output.mutable_motiontarget()->mutable_position()->set_rz(target_rz);
+            // not done, consider the ball vector next
+        }
+
+        // Check if there is a ball, if not, do nothing (target is the center of the goal)
+        if (!input.worldstate().has_ball())
+        {
+            return error_value;
+        }
+
+        // There is a ball, let's move along with it, clipping to keeper range
+        double ball_x = input.worldstate().ball().position().x();
+        output.mutable_motiontarget()->mutable_position()->set_x(clip_x(ball_x, params.maxmovex()));
+
+        // TODO: port more of existing motionplanning code
 
         // Check if keeper needs to respond to a ball
         // * using ball speed
@@ -67,7 +116,6 @@ int FalconsActionKeeper::FalconsActionKeeper::tick
         }
 
         // Check if the ball is moving towards the robot
-        MRA::Geometry::Position robot_position(input.worldstate().robot().position());
         MRA::Geometry::Velocity ball_velocity_rcs = ball_velocity;
         ball_velocity_rcs.transformFcsToRcs(robot_position);
         bool ballmovingtowardsrobot = ball_velocity_rcs.y < 0;
@@ -94,8 +142,8 @@ int FalconsActionKeeper::FalconsActionKeeper::tick
 
         // Set the calculated interception point as the motion target, facing the ball
         MRA::Geometry::Position robot_target_position(intersect_point.x, intersect_point.y);
-        robot_target_position.rz = 0.0; // facing forward into the field
-        output.mutable_motiontarget()->mutable_position()->set_x(robot_target_position.x);
+        robot_target_position.rz = target_rz;
+        output.mutable_motiontarget()->mutable_position()->set_x(clip_x(robot_target_position.x, params.maxmovex()));
         output.mutable_motiontarget()->mutable_position()->set_y(robot_target_position.y);
         output.mutable_motiontarget()->mutable_position()->set_rz(robot_target_position.rz);
     }

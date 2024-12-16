7c7fe75c72e5ec5a157d7df72e2b9f049e14bb0c
Jurge vanEijck <jurge.van.eijck@gmail.com>
2024-12-16 22:55:50 +0100

    remove dependency with Boost

--- components/robotsports/role_assigner/test/xml_tester++.cpp
+++ components/robotsports/role_assigner/test/xml_tester++.cpp
@@ -14,35 +14,21 @@
 #include <wait.h>
 #include <math.h>
 #include <signal.h>
-#include <boost/program_options.hpp>
 
 #include "xmlRoleAssigner.hpp"
 
-namespace po = boost::program_options;
 using namespace std;
 
 
 int main(int argc, char *argv[]) {
     try {
         // Arguments will be stored here
-        std::string input_file;
+        if (argc != 2) {
+            cout << "usage:" << endl;
+            cout << "\t" << argv[0] << " <input-file>" << endl;
 
-        // Configure options here
-        po::options_description desc ("Allowed options");
-        desc.add_options ()
-                ("help,h", "print usage message")
-                ("input-file,i", po::value(&input_file), "Input file");
-
-        // Parse command line arguments
-        po::variables_map vm;
-        po::store (po::command_line_parser (argc, argv).options (desc).run (), vm);
-        po::notify (vm);
-        // Check if --input is missing or --help is given
-        if (vm.count ("help") || !vm.count ("input-file")) {
-            std::cerr << desc << "\n";
-            return 1;
         }
-
+        std::string input_file = argv[1];
         // call xml planner with the received parameters
         role_assigner_with_xml_input(input_file);
     }

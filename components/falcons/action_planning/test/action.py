22772a1403f5a3773113ef2cf812888768ed072f
Jan Feitsma <jan.feitsma@gmail.com>
2025-01-28 18:37:03 +0100

    falcons/action_planning: exted diagnostics plotter with a second window for showing timelines

--- components/falcons/action_planning/test/action.py
+++ components/falcons/action_planning/test/action.py
@@ -1,6 +1,7 @@
 # python modules
 import struct
 import json
+import numpy as np
 from datetime import datetime
 from matplotlib import pyplot as plt
 from google.protobuf.json_format import MessageToDict
@@ -20,6 +21,21 @@ def protobuf_enum2str(pb_object, field_name) -> str:
     # TODO: this utility function should perhaps be shared at a common level
 
 
+def timestamp_to_str(timestamp):
+    """
+    Convert timestamp to string.
+    """
+    timestamp_str = datetime.fromtimestamp(timestamp.seconds + timestamp.nanos / 1e9).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+    return timestamp_str
+
+
+def timestamp_to_float(timestamp):
+    """
+    Convert timestamp to float.
+    """
+    return timestamp.seconds + timestamp.nanos / 1e9
+
+
 class Action:
     """
     Class to handle an action file, covering a series of samples, typically leading to a result PASSED/FAILED.
@@ -29,8 +45,8 @@ class Action:
         self.data = None
         self.tickplotter = ActionTick()
         self.sample_idx = 0
-        self.fig = None
-        self.ax = None
+        self.fig_f = None
+        self.ax_f = None
 
     def load(self, datafile: str):
         """
@@ -44,34 +60,65 @@ class Action:
         self.data.MergeFromString(raw_data)
         self.action_type_str = protobuf_enum2str(self.data, 'type')
 
-    def on_key_press(self, event):
+    def on_key_press_timeline(self, event):
         """
-        Keypress event handler.
+        Keypress event handler for the timeline window.
+        """
+        if event.key == 'q':
+            plt.close('all')
+
+    def on_key_press_field(self, event):
+        """
+        Keypress event handler for the field window.
         """
         # graceful shutdown?
         if event.key == 'q':
-            plt.close()
+            plt.close('all')
             return
         if event.key == 'right':
             self.sample_idx = (self.sample_idx + 1) % len(self.data.samples)
         elif event.key == 'left':
             self.sample_idx = (self.sample_idx - 1) % len(self.data.samples)
-        self.ax.clear()
-        plot_field(self.ax)
+        self.ax_f.clear()
+        plot_field(self.ax_f)
         self.plot_sample(self.sample_idx)
-        self.fig.canvas.draw()
+        self.fig_f.canvas.draw()
 
-    def plot(self):
+    def plot_field(self, maximize=False, show=True):
         """
-        Setup the window and connect key_press events.
+        Setup the field window and connect key_press events.
         """
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        plot_field(self.ax)
-        self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+        self.fig_f = plt.figure()
+        self.ax_f = self.fig_f.add_subplot(111)
+        plot_field(self.ax_f)
+        self.fig_f.canvas.mpl_connect('key_press_event', self.on_key_press_field)
         self.plot_sample(-1) # start with the last one
-        figManager = plt.get_current_fig_manager()
-        self.fig.canvas.manager.full_screen_toggle()
+        if maximize:
+            figManager = plt.get_current_fig_manager()
+            self.fig_f.canvas.manager.full_screen_toggle()
+        if show:
+            self.show()
+
+    def plot_timeline(self, show=True):
+        """
+        Setup the timeline window.
+        """
+        data_signals = DataSignals(self.data.samples)
+        self.fig_t = plt.figure()
+        self.fig_t.canvas.mpl_connect('key_press_event', self.on_key_press_timeline)
+        self.ax_t = self.fig_t.add_subplot(111)
+        self.ax_t.plot(data_signals.t, data_signals.values_target['x'], label='target.x')
+        self.ax_t.plot(data_signals.t, data_signals.values_target['y'], label='target.y')
+        self.ax_t.plot(data_signals.t, data_signals.values_target['rz'], label='target.rz')
+        self.ax_t.plot(data_signals.t, data_signals.values_target['d'], label='target.d')
+        self.ax_t.legend()
+        if show:
+            self.show()
+
+    def show(self):
+        """
+        Show all figures.
+        """
         plt.show()
 
     def plot_sample(self, sample_idx):
@@ -80,17 +127,10 @@ class Action:
         """
         sample_idx %= len(self.data.samples)
         sample = self.data.samples[sample_idx]
-        self.tickplotter.plot_io(sample.input, sample.output, self.ax)
+        self.tickplotter.plot_io(sample.input, sample.output, self.ax_f)
         action_result_str = protobuf_enum2str(self.data.samples[sample_idx].output, 'actionresult')
-        ts = self.timestamp_to_str(sample.timestamp)
+        ts = timestamp_to_str(sample.timestamp)
         plt.title(f'action={self.action_type_str} status={action_result_str} sample={1+sample_idx}/{len(self.data.samples)} time={ts}')
-
-    def timestamp_to_str(self, timestamp):
-        """
-        Convert timestamp to string.
-        """
-        timestamp_str = datetime.fromtimestamp(timestamp.seconds + timestamp.nanos / 1e9).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-        return timestamp_str
 
     def print_info(self):
         """
@@ -165,7 +205,7 @@ class ActionTick:
         """
         self.data = Data(datafile)
 
-    def plot(self, ax: plt.Axes = None):
+    def plot_field(self, ax: plt.Axes = None):
         """
         Plot the situation.
 
@@ -196,3 +236,32 @@ class ActionTick:
         if output.HasField('setpoints') and output.setpoints.HasField('move'):
             targetpos = output.setpoints.move.target
             plot_target(ax, targetpos, input.worldState.robot)
+
+
+def extract_target(sample_data, in_rcs=False):
+    result = {'enabled': True}
+    empty_values = np.full(len(sample_data), np.nan)
+    # 4 separate instances
+    result['x'] = empty_values.copy()
+    result['y'] = empty_values.copy()
+    result['rz'] = empty_values.copy()
+    result['d'] = empty_values.copy()
+    for it, sample in enumerate(sample_data):
+        if sample.output.setpoints.HasField('move'):
+            robot = sample.input.worldState.robot
+            targetpos = sample.output.setpoints.move.target.position
+            result['x'][it] = targetpos.x - robot.position.x * in_rcs
+            result['y'][it] = targetpos.y - robot.position.y * in_rcs
+            result['rz'][it] = targetpos.rz - robot.position.rz * in_rcs
+            result['d'][it] = ((robot.position.x - targetpos.x)**2 + (robot.position.y - targetpos.y)**2)**0.5
+    return result
+
+
+class DataSignals:
+    """
+    Class to extract a set of signals from action samples, for plotting timelines.
+    """
+    def __init__(self, sample_data):
+        t0 = timestamp_to_float(sample_data[0].timestamp)
+        self.t = [timestamp_to_float(sample.timestamp) - t0 for sample in sample_data]
+        self.values_target = extract_target(sample_data, in_rcs=True)
--- components/falcons/action_planning/test/plot.py
+++ components/falcons/action_planning/test/plot.py
@@ -33,23 +33,29 @@ def parse_args(args: list) -> argparse.Namespace:
     class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
         pass
     parser = argparse.ArgumentParser(description=descriptionTxt, epilog=exampleTxt, formatter_class=CustomFormatter)
+    parser.add_argument('-f', '--field', action='store_true', help='only plot field sample browser, omit the timeline plot')
+    parser.add_argument('-t', '--time', action='store_true', help='only plot timeline, omit the field plot')
     parser.add_argument('datafile', help='data file to load')
     return parser.parse_args(args)
 
 
 def main(args: argparse.Namespace) -> None:
     """
-    Make the plot.
+    Make the plot(s).
     """
+    a = None
     if args.datafile.endswith('.bin') and 'tick' in args.datafile:
         a = action.ActionTick()
         a.load(args.datafile)
-        a.plot()
     else:
         a = action.Action()
         a.load(args.datafile)
         a.print_info()
-        a.plot()
+    if not args.field:
+        a.plot_timeline(show=False)
+    if not args.time:
+        a.plot_field(show=False)
+    a.show()
 
 
 if __name__ == '__main__':

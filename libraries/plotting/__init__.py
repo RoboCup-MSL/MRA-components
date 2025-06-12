3b03104ca1cacfb23295412948f9031b94c315fd
Jan Feitsma <jan.feitsma@gmail.com>
2025-06-12 18:31:43 +0200

    wipe things which are likely obsolete or must be reconsidered for ROS2

--- libraries/plotting/__init__.py
+++
@@ -1,8 +0,0 @@
-from .worldstate import plot as plot_worldstate
-from .field import plot as plot_field
-from .ball import plot as plot_ball
-from .robot import plot as plot_robot
-from .obstacle import plot as plot_obstacle
-from .target import plot as plot_target
-
-__all__ = ['plot_worldstate', 'plot_field', 'plot_robot', 'plot_ball', 'plot_obstacle', 'plot_target']
--- libraries/plotting/obstacle.py
+++
@@ -1,5 +0,0 @@
-from matplotlib.patches import Circle
-
-
-def plot(ax, obstacle, color='k'):
-    ax.add_patch(Circle((obstacle.x, obstacle.y), 0.1, fill=True, color=color, zorder=3.1))

057a2f59479d9c4776088211055a7cd12567b527
Jan Feitsma <jan.feitsma@gmail.com>
2025-06-19 20:28:48 +0200

    falcons/action_get_ball: reimplement in ros2, rename from action_fetch_ball, no point in keeping that separated anymore

---
+++ components/falcons/action_get_ball/src/action_get_ball.cpp
@@ -0,0 +1,65 @@
+#include "action_get_ball.hpp"
+#include <cmath>
+
+ActionGetBall::ActionGetBall()
+: ActionBase("getball", mra_common_msgs::msg::ActionType::ACTION_GETBALL)
+{
+}
+
+void ActionGetBall::tick(const mra_common_msgs::msg::WorldState& world_state, const input_type& input)
+{
+    // Reset verdict and result
+    verdict_.clear();
+    actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_INVALID;
+
+    // Example: always enable ballhandlers (if output msg supports it)
+    // output.bh_enabled = true;
+
+    // Check if robot has the ball
+    if (world_state.robot.hasball) {
+        actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_PASSED;
+        verdict_ = "robot has the ball";
+        return;
+    }
+
+    // Fail if robot is inactive
+    if (!world_state.robot.active) {
+        actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_FAILED;
+        verdict_ = "robot is inactive";
+        return;
+    }
+
+    // Fail if no ball
+    if (!world_state.balls.size()) {
+        actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_FAILED;
+        verdict_ = "robot lost track of the ball";
+        return;
+    }
+
+    // Fail if teammate has the ball
+    for (const auto& teammate : world_state.teammates) {
+        if (teammate.hasball) {
+            actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_FAILED;
+            verdict_ = "teammate got the ball";
+            return;
+        }
+    }
+
+    // Fail if ball is too far away
+    double dx = world_state.balls[0].pose.position.x - world_state.robot.pose.position.x;
+    double dy = world_state.balls[0].pose.position.y - world_state.robot.pose.position.y;
+    double dist = std::sqrt(dx*dx + dy*dy);
+    double action_radius = config_.radius;
+    if (input.radius > 0.0) {
+        action_radius = input.radius;
+    }
+    if (dist > action_radius) {
+        actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_FAILED;
+        verdict_ = "ball too far away";
+        return;
+    }
+
+    // Otherwise, keep running
+    actionresult_ = mra_common_msgs::msg::ActionResult::ACTIONRESULT_RUNNING;
+    verdict_ = "fetching ball";
+}

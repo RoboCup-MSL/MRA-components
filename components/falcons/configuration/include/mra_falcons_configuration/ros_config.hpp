b56f3b423532bf7914e23ebe70437876322ac8c1
Jan Feitsma <jan.feitsma@gmail.com>
2025-06-26 20:40:50 +0200

    falcons/configuration: switch to reliable QoS, because I spotted occasional hanging of the simulator, likely due to some kind of message dropping

--- components/falcons/configuration/include/mra_falcons_configuration/ros_config.hpp
+++ components/falcons/configuration/include/mra_falcons_configuration/ros_config.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
-#define FALCONS_ROS_QOS 10
+#define FALCONS_ROS_QOS make_qos()
+
+#include <rclcpp/rclcpp.hpp>
+#include <rmw/rmw.h>
+
+inline rclcpp::QoS make_qos()
+{
+    // Setup a QoS profile
+    rclcpp::QoS qos(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
+    qos.reliable();
+    qos.keep_last(100);
+    qos.transient_local();
+    return qos;
+}
